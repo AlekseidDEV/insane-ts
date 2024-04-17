d62f2ed53b59d685d35ceb3ae764cebff5286397
Alex <lesha.rudiko@gmail.com>
2024-04-17 17:24:15 +0300

    new project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+
+    module: {
+        rules: [
+            {
+             test: /\.ts$/, 
+             use: 'ts-loader',
+             exclude: /node_modules/
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+      },
+}
