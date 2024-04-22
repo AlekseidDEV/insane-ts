77d8af896badde53b685b2f0ce51fc0644a7891b
Alex <lesha.rudiko@gmail.com>
2024-04-22 10:02:40 +0300

    accordoin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import { Elem, Form } from "./models/aliases";
 import { numAccordion } from "./modules/accordNumber";
+import { accordion } from "./modules/accordion";
 import { formSend } from "./modules/formSend";
 import { maskPhone } from "./modules/helpers/inputMask";
 import { menuFunc } from "./modules/menu";
@@ -22,18 +23,7 @@ if(window.innerWidth > 576){
     menuFunc("translate3d(0px, 0px, 0px)", "translate3d(0px, 750px, 0px)")
 }
 
-document.querySelector('.button-footer')?.addEventListener('click', (e) => {
-    e.preventDefault()
-    smoothScroll((e.target as HTMLAnchorElement).hash)
-})
-
 maskPhone('input[name="phone"]')
-
-document.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    formSend(e, (e.target as Form))
-})
 
 tips()
 mobileTips()
@@ -42,6 +32,7 @@ repairSliderMob("#nav-arrow-repair-left_base", '#nav-arrow-repair-right_base')
 portfolioSlider(".portfolio-slider-wrap", "#portfolio-arrow_right", "#portfolio-arrow_left", ".portfolio-slider__slide-frame", '.portfolio-slider__slide.fade-tab', '.slider_track')
 transparancy()
 sliderReviews()
+accordion()
 
 document.addEventListener('click', (e) => {
     if((e.target as Elem).matches('.button.button_wide')){
@@ -49,5 +40,16 @@ document.addEventListener('click', (e) => {
     } else if((e.target as Elem).matches(".link-privacy")){
         openPopup('.popup-privacy', '.close-privacy')
     }
+})
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    formSend(e, (e.target as Form))
+})
+
+document.querySelector('.button-footer')?.addEventListener('click', (e) => {
+    e.preventDefault()
+    smoothScroll((e.target as HTMLAnchorElement).hash)
 })
 
---
+++ src/modules/accordion.ts
@@ -0,0 +1,20 @@
+import { Div, Elem } from "src/models/aliases"
+
+export const accordion = () => {
+    const accordionBlock: Div | null = document.querySelector('.accordion_faq')
+    const titleBlocks = accordionBlock?.querySelectorAll('.title_block')
+
+    const changeAccordion = (e: Event) => {
+        if((e.target as Elem).matches('.title_block')){
+            titleBlocks?.forEach((block) => {
+                if(e.target === block){
+                    block.classList.toggle('msg-active')
+                } else{
+                    block.classList.remove('msg-active')
+                }
+            })
+        }
+    }
+
+    accordionBlock?.addEventListener('click', changeAccordion)
+}
